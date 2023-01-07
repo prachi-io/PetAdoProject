48b0fd55f627d6c50f5976437829e28762a57854
Prachi Aggarwal <prachi2002aggarwal@gmail.com>
2023-01-08 02:17:52 +0530

    people who adopted

--- frontend/src/components/AdoptedInfo.js
+++ frontend/src/components/AdoptedInfo.js
@@ -1,4 +1,37 @@
 import React from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 export default function AdoptedInfo() {
-    return(<h1>adopted info</h1>)
+
+    const [userData,setUserData] = useState("")
+
+    const fetchUserData = async () => {
+        const resp = await axios.get("http://localhost:4000/api/adoption/getAll")
+        if(resp.data.length > 0) {
+            setUserData(resp.data)
+            console.log(resp.data)
+        }
+    }
+
+
+    useEffect(()=> {
+        fetchUserData().then( )
+        console.log(userData)
+    })
+
+
+    return (
+        <>
+            {userData && userData.map((user) => (
+                <div className="w-full rounded-lg shadow-md lg:max-w-sm m-auto">
+                    <div className="p-4">
+                        <h4 className="text-xl font-semibold tracking-tight text-blue-600">
+                            {user.firstName + user.lastName}
+                        </h4>
+                    </div>
+                </div>
+            ))}
+        </>
+    )
 }
